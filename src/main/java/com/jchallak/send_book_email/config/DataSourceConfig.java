e410a94d55e8a404a6df3b889f028e3071e00bbf
Mzg. <jchallaksantos@gmail.com>
2025-04-08 14:30:40 -0300

    DataSourceConfig

---
+++ src/main/java/com/jchallak/send_book_email/config/DataSourceConfig.java
@@ -0,0 +1,39 @@
+package com.jchallak.send_book_email.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Primary
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource springDS(){
+        return DataSourceBuilder.create().build();
+    }
+
+
+    @Bean
+    @ConfigurationProperties(prefix = "app.datasource")
+    public DataSource appDS(){
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManagerApp(@Qualifier("appDS") DataSource dataSource){
+        return new DataSourceTransactionManager(dataSource);
+
+    }
+
+
+
+}
